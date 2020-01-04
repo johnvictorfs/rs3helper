aa26fabaa4c5f25f6edb2fd434088634cbfb89c4
John Victor Sampaio <johnvictorfs@gmail.com>
2020-01-04 20:38:11 -0300

    Added things

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
+import platform
+
+from PIL import Image
 import numpy as np
+import pytesseract
 import cv2
 import mss
 
+import re
+from typing import Tuple, Optional
 
-def find_match(image, file_name, text, threshold=0.7):
+
+operating_system = platform.system().lower()
+
+if operating_system == "darwin":
+    from mss.darwin import MSS
+elif operating_system == "linux":
+    from mss.linux import MSS
+else:
+    from mss.windows import MSS
+
+
+def find_match(image, file_name: str, text: str, threshold: float = 0.7):
     template = cv2.imread(file_name, cv2.IMREAD_UNCHANGED)
     # template = cv2.cvtColor(template, cv2.COLOR_BGR2RGB)
     template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
@@ -28,69 +45,97 @@ def find_match(image, file_name, text, threshold=0.7):
     # https://stackoverflow.com/a/51713648
     _, max_val, _, max_loc = cv2.minMaxLoc(result)
     top_left = max_loc
-    # bottom_right = (top_left[0] + w, top_left[1] + h)
+    bottom_right = (top_left[0] + 95, top_left[1] + 19)
 
     for pt in zip(*loc[::-1]):
         # nox_text = cv2.imread('nox_staff_text.png', 1)
         # nox_text = cv2.cvtColor(nox_text, cv2.COLOR_BGR2GRAY)
         try:
             # cv2.imshow('staff', nox_text)
-            font = cv2.FONT_HERSHEY_SIMPLEX
             # top_left[1] += 20
-            cv2.putText(image, text, top_left, font, 0.8, (0, 255, 0), 2, cv2.LINE_AA)
+
+            # Add label at match
+            # font = cv2.FONT_HERSHEY_SIMPLEX
+            # cv2.putText(image, text, top_left, font, 0.8, (0, 255, 0), 2, cv2.LINE_AA)
+
             # image[(pt[1] - 25):(pt[1] - 25) + nox_text.shape[0], pt[0]:pt[0] + nox_text.shape[1]] = nox_text
-            # cv2.rectangle(image, top_left, bottom_right, (0, 255, 0), 2)
+            cv2.rectangle(image, top_left, bottom_right, (0, 255, 0), 2)
             # cv2.rectangle(image, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
         except Exception:
             ...
-            # print(e)
 
 
-def read_quest_thing():
-    import subprocess
-    from pathlib import Path
-    from PIL import Image
+def find_hp_text(image, file_name: str, text: str, threshold: float = 0.7):
+    template = cv2.imread(file_name, cv2.IMREAD_UNCHANGED)
+    template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
 
-    orig = Image.open("test.png")
-    orig_pix = orig.load
+    result = cv2.matchTemplate(image, template, cv2.TM_CCOEFF_NORMED)
 
-    new = Image.new(orig.mode, orig.size)
-    new_pix = new.load()
+    _, _, _, (left, upper) = cv2.minMaxLoc(result)
 
-    def pixels(img):
-        width, height = img.size
-        for x in range(width):
-            for y in range(height):
-                yield x, y
+    left += 25
+    upper -= 2
 
-    new.save("out.png")
+    right = left + 75
+    lower = upper + 21
 
-    subprocess.run("tesseract out.png string", shell=True)
-    string = Path('string.txt').read_text()
-    print(string.splitlines()[0])
+    cropped = Image.fromarray(image)
+
+    cropped = cropped.crop((left, upper, right, lower))
+
+    cv2.imshow('cropped', np.array(cropped))
+
+    return cropped
+
+
+def read_text(image):
+    resized_image = cv2.resize(image, (0, 0), fx=2, fy=2)
+    resized_image = Image.fromarray(resized_image)
+    return pytesseract.image_to_string(resized_image, config='--psm 7')
+
+
+def format_hp(hp_text: str) -> Optional[Tuple[int, int]]:
+    """
+    Gets current and max hp from hp bar string, ignoring other characters
+
+    Example:
+    '@@ 9900/9900' -> (9900, 9900)
+    """
+    match = re.search(r"(\d+)/(\d+)", hp_text)
+
+    if match:
+        current_hp, max_hp = match.groups()
+
+        return int(current_hp), int(max_hp)
+    return None
 
 
 if __name__ == '__main__':
-    read_quest_thing()
-
-    import sys
-    sys.exit(0)
-
+    sct: MSS
     with mss.mss() as sct:
         monitor = {"top": 400, "left": 600, "width": 800, "height": 640}
+
         while True:
             # Get raw pixels from the screen, save it to a Numpy array
             frame = np.array(sct.grab(monitor))
-            # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-            find_match(frame, 'nox_staff.png', 'Noxious Staff')
-            find_match(frame, 'ovl.png', 'Overload Timer')
-            # if not match:
-            #     match = find_match(frame, 'nox_staff.png')
-            #     if not match:
-            #         match = find_match(frame, 'nox_staff.png')
-            cv2.imshow('frame', frame)
+            hp_text = find_hp_text(frame, 'images/hp_icon.png', 'HP Text')
+            hp_bar = read_text(np.array(hp_text))
+
+            formatted_hp = format_hp(hp_bar)
+
+            if formatted_hp:
+                current_hp, max_hp = formatted_hp
+
+                if current_hp < 9900:
+                    import datetime
+                    now = datetime.datetime.now()
+
+                    print(f'[DANGER] {now.second} - Current HP at {current_hp}')
+
+            # cv2.imshow('frame', frame)
 
             # Press "q" to quit
             if cv2.waitKey(1) & 0xFF == ord("q"):
